7e03e428955c36db2b6f4d57ba0d0e80a494fe29
GOGO98901 <gogo98901@live.co.uk>
2016-10-11 18:27:12 +0100

    added express to stop heroku from crashing the app

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+var express = require('express')
+var app = express()
+
 var Discord = require('discordie');
 
 const Events = Discord.Events;
@@ -19,3 +22,16 @@ client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
 		e.message.channel.sendMessage("Pong");
 	}
 });
+
+
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+	response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+	console.log("Node app is running at localhost:" + app.get('port'))
+})
